d60dd69a66f29ed9d4b3e18063cbb6648686ff08
AnaKlarissa <anaklarisa16@gmail.com>
2025-09-01 20:28:26 -0300

    Create array.py

---
+++ array.py
@@ -0,0 +1,9 @@
+# array-ana-klarissa
+# nomes
+nomes = ["Maria", "Cassia", "Jonas", "Pedro", "Matheus"]
+
+print("O primeiro elemento do array", nomes[0])
+print("O segundo elemento do array", nomes[1])
+print("O terceiro elemento do array", nomes[2])
+print("O quarto elemento do array", nomes[3])
+print("O quinto elemento do array", nomes[4])
